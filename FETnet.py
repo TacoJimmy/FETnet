4a1add7b679a384ab57c615fa5c6b81ce66d514b
TacoJimmy <t5669016@ntut.org.tw>
2024-09-11 12:05:04 +0800

    Update FETnet.py

--- FETnet.py
+++ FETnet.py
@@ -48,6 +48,36 @@ def FET_Publish(Meter_data):
              "data":[
                  {"timestemp":timestamp,
                   "values":{
+                      "voltage_r_s":380,
+                      }}]}
+            ]
+    
+        data03 = client.publish('/ShangriLa2024TPE/v1/telemetry/infilink',json.dumps(mod_payload))
+        time.sleep(10)
+        print (data03)
+        print (mod_payload)
+    except:
+        pass
+'''
+def FET_Publish(Meter_data):
+    try:
+        now = datetime.datetime.now()
+        timestamp = int(now.timestamp())
+        client = mqtt.Client('', True, None, mqtt.MQTTv31)
+        client.username_pw_set('infilink_ShangriLa2024TPE', 'wCGTd25n')
+        client.tls_set(cert_reqs=ssl.CERT_NONE)
+        client.connect('mqtt-device.fetiot3s1.fetnet.net', 8884 , 60)
+        client.loop_start()
+        time.sleep(1)
+        client.on_connect
+        time.sleep(1)
+        mod_payload = [
+            {"access_token":"64688f1c2fe5453998c6c475eddbe5ac",
+             "app":"ShangriLa2024TPE",
+             "type":"electricity_meter",
+             "data":[
+                 {"timestemp":timestamp,
+                  "values":{
                       "voltage_r_s":Meter_data[1],
                       "voltage_s_t":Meter_data[2],
                       "voltage_t_r":Meter_data[3],
@@ -73,10 +103,11 @@ def FET_Publish(Meter_data):
         print (mod_payload)
     except:
         pass
-
+'''
 def do_job():
     
-    PowerMeter = MBus.read_3p3w_meter('/dev/ttyS3',30,1)
+    #PowerMeter = MBus.read_3p3w_meter('/dev/ttyS3',30,1)
+    PowerMeter = 0
     FET_Publish(PowerMeter)
 
 #schedule.every(5).minutes.do(do_job)

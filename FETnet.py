0d3692867f8e98c6e3f1f004a3b600727043d683
TacoJimmy <t5669016@ntut.org.tw>
2024-09-12 17:56:50 +0800

    Update FETnet.py

--- FETnet.py
+++ FETnet.py
@@ -144,6 +144,8 @@ schedule.every(5).minutes.do(do_job)
 
 if __name__ == "__main__":
     while True:
-        
-        schedule.run_pending()
-        time.sleep(1)
+        try:
+            schedule.run_pending()
+            time.sleep(1)
+        except:
+            pass

b9a7cc02f7dbf645a8a34642699cd1503a010b50
TacoJimmy <t5669016@ntut.org.tw>
2024-09-13 14:30:56 +0800

    Update flowMeter.py

--- flowMeter.py
+++ flowMeter.py
@@ -9,7 +9,7 @@ from modbus_tk import modbus_rtu
 import struct
 
 
-SERIAL_PORT = 'COM6'  
+SERIAL_PORT = '/dev/ttyS7'   
 BAUDRATE = 9600             
 PARITY = 'N'                
 STOPBITS = 1                
@@ -32,20 +32,23 @@ def flow_meter(PORT,ID,loop):
         master.set_verbose(True)
         
         FlowMeter1 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 0, 2)
-        RT_Flowdata = round(float_num(FlowMeter1[0], FlowMeter1[1]),2)
+        RT_Flowdata = round(float_num(FlowMeter1[0], FlowMeter1[1])*16.667,2) #單位LPM
         FlowMeter2 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 2, 2)
-        RT_Energydata = round(float_num(FlowMeter2[0], FlowMeter2[1]),2)
+        RT_EnergyGJ = round(float_num(FlowMeter2[0], FlowMeter2[1]),2)
+        RT_EnergyRTh = round(RT_EnergyGJ*8.33)
         FlowMeter3 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 4, 2)
         RT_FlowSpeeddata = round(float_num(FlowMeter3[0], FlowMeter3[1]),2)
-        FlowMeter4 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 6, 2)
-        RT_FlowSpeeddata = round(float_num(FlowMeter4[0], FlowMeter4[1]),2)
-        FlowMeter5 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 8, 2)
-        RT_FlowSpeeddata = round(float_num(FlowMeter5[0], FlowMeter5[1]),2)
-        FlowMeter6 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 10, 2)
-        RT_FlowSpeeddata = round(float_num(FlowMeter6[0], FlowMeter6[1]),2)
-        FlowMeter7 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 12, 2)
-        RT_FlowSpeeddata = round(float_num(FlowMeter7[0], FlowMeter7[1]),2)
-        
+        #FlowMeter4 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 6, 2)
+        #RT_FlowSpeeddata = round(float_num(FlowMeter4[0], FlowMeter4[1]),2)
+        FlowMeter5 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 10, 2)
+        Cal_volume = round(float_num(FlowMeter5[0], FlowMeter5[1]),2)
+        FlowMeter6 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 18, 2)
+        Cal_EnergyGJ = round(float_num(FlowMeter6[0], FlowMeter6[1]),2)
+        Cal_EnergyRTH = round(Cal_EnergyGJ*83.33)
+        FlowMeter7 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 32, 2)
+        RT_FlowTemp1 = round(float_num(FlowMeter7[0], FlowMeter7[1]),2)
+        FlowMeter7 = master.execute(ID, cst.READ_HOLDING_REGISTERS, 34, 2)
+        RT_FlowTemp2 = round(float_num(FlowMeter7[0], FlowMeter7[1]),2)
         
         #RT_Flow = master.execute(ID, cst.READ_HOLDING_REGISTERS, 34, 2)
         #RT_Flowdata = round(float_num(RT_Flow[0], RT_Flow[1]),2)
@@ -58,7 +61,7 @@ def flow_meter(PORT,ID,loop):
         
         
         
-        return (RT_Flowdata,RT_Energydata,RT_FlowSpeeddata,Temp11,Temp21)
+        return ()
         #return (RT_Flowdata,RT_Energydata,RT_FlowSpeeddata,Temp11,Temp21)
 
     except:
@@ -66,4 +69,4 @@ def flow_meter(PORT,ID,loop):
 
 
 if __name__ == '__main__':
-    print(flow_meter('COM6',22,1))
+    print(flow_meter('/dev/ttyS7' ,22,1))

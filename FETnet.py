1865bd513064dcfbd7ded27a72dd9ae7dbcbbfe7
TacoJimmy <t5669016@ntut.org.tw>
2024-09-13 14:57:34 +0800

    Update FETnet.py

--- FETnet.py
+++ FETnet.py
@@ -120,27 +120,73 @@ def FET_Publish_Station(Meter_data,access_token,timestamp):
     except:
         pass
 
+def FlowMeter_Publish_Station(Meter_data,access_token,timestamp):
+    try:
+        now = datetime.datetime.now()
+        timestamp = int(now.timestamp())
+        client = mqtt.Client('', True, None, mqtt.MQTTv31)
+        client.username_pw_set('infilink_ShangriLa2024TPE', 'wCGTd25n')
+        client.tls_set(cert_reqs=ssl.CERT_NONE)
+        client.connect('mqtt-device.fetiot3s1.fetnet.net', 8884 , 60)
+        client.loop_start()
+        time.sleep(1)
+        client.on_connect
+        time.sleep(1)
+        mod_payload = [
+            {"access_token":access_token,
+             "app":"ShangriLa2024TPE",
+             "type":"electricity_meter",
+             "data":[
+                 {"timestemp":timestamp,
+                  "values":{
+                      "flowmeter_rt_volume_flow_rate":Meter_data[0],
+                      "flowmeter_rt_energy_gjhr":Meter_data[1],
+                      "flowmeter_rt_energy_rth":Meter_data[2],
+                      "flowmeter_rt_flow_rate":Meter_data[3],
+                      "flowmeter_total_volume_flow_rate":Meter_data[4],
+                      "flowmeter_total_energy_gjhr":Meter_data[5],
+                      "flowmeter_total_energy_rth":Meter_data[6],
+                      "flowmeter_temperature_inlet":Meter_data[7],
+                      "flowmeter_temperature_outlet":Meter_data[8],
+                      "alive":Meter_data[9]
+                      }}]}
+            ]
+    
+        data03 = client.publish('/ShangriLa2024TPE/v1/telemetry/infilink',json.dumps(mod_payload))
+        time.sleep(10)
+        print (data03)
+        print (mod_payload)
+    except:
+        pass
+
 def do_job():
     
-    now = datetime.datetime.now()
-    timestamp = int(now.timestamp())
+    try:
+        now = datetime.datetime.now()
+        timestamp = int(now.timestamp())
     
-    PowerMeter = MBus.read_3p3w_meter('/dev/ttyS3',30,1)
-    FET_Publish_Product(PowerMeter,"4ebb30b3db7546b194334f7a0188b487",timestamp)
-    FET_Publish_Station(PowerMeter,"4ebb30b3db7546b194334f7a0188b487",timestamp)
+        PowerMeter = MBus.read_3p3w_meter('/dev/ttyS3',30,1)
+        FET_Publish_Product(PowerMeter,"4ebb30b3db7546b194334f7a0188b487",timestamp)
+        FET_Publish_Station(PowerMeter,"4ebb30b3db7546b194334f7a0188b487",timestamp)
     
-    PowerMeter = MBus.read_3p3w_meter('/dev/ttyS3',31,1)
-    FET_Publish_Product(PowerMeter,"84f4d26e14bc45ddab170a48b9cc1e10",timestamp)
-    FET_Publish_Station(PowerMeter,"84f4d26e14bc45ddab170a48b9cc1e10",timestamp)
+        PowerMeter = MBus.read_3p3w_meter('/dev/ttyS3',31,1)
+        FET_Publish_Product(PowerMeter,"84f4d26e14bc45ddab170a48b9cc1e10",timestamp)
+        FET_Publish_Station(PowerMeter,"84f4d26e14bc45ddab170a48b9cc1e10",timestamp)
     
-    PowerMeter = MBus.read_3p3w_meter('/dev/ttyS3',32,1)
-    FET_Publish_Product(PowerMeter,"99b270cf07544505a91fe924062af584",timestamp)
-    FET_Publish_Station(PowerMeter,"99b270cf07544505a91fe924062af584",timestamp)
+        PowerMeter = MBus.read_3p3w_meter('/dev/ttyS3',32,1)
+        FET_Publish_Product(PowerMeter,"99b270cf07544505a91fe924062af584",timestamp)
+        FET_Publish_Station(PowerMeter,"99b270cf07544505a91fe924062af584",timestamp)
     
-    PowerMeter = MBus.read_3p3w_meter('/dev/ttyS7',12,1)
-    FET_Publish_Product(PowerMeter,"38e20a608eae40f49e2a1f1f6f286fea",timestamp)
-    FET_Publish_Station(PowerMeter,"38e20a608eae40f49e2a1f1f6f286fea",timestamp)
-    
+        PowerMeter = MBus.read_3p3w_meter('/dev/ttyS7',12,1)
+        FET_Publish_Product(PowerMeter,"38e20a608eae40f49e2a1f1f6f286fea",timestamp)
+        FET_Publish_Station(PowerMeter,"38e20a608eae40f49e2a1f1f6f286fea",timestamp)
+
+        PowerMeter = MBus.read_3p3w_meter('/dev/ttyS7',12,1)
+        FlowMeter_Publish_Station(PowerMeter,"9382460cc8534e368589b0956a859f9f",timestamp)
+    except:
+            pass
+
+
 schedule.every(5).minutes.do(do_job)
 #schedule.every(60).seconds.do(do_job)
 
